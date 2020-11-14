0d162545e71c6c0170ac8f2980eae25f2b7814d9
adimov <nasko312@gmail.com>
2020-11-14 15:06:10 +0200

    Upload project
    Made Movie model,controller and migrations.
    Add movie basic functionality done.
    View movie basic functionality done.

---
+++ moviedb/database/migrations/2020_11_13_183235_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->string('title')->unique();
+            $table->time('timespan');
+            $table->string('description');
+            $table->date('published_at');
+            $table->string('genres');
+            $table->string('poster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
