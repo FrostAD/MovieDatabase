912aebdab0396f6b14393f9c594ebf7b8f69a54e
RobertStoyanov0210 <rstoianov0210@gmail.com>
2020-12-02 18:29:52 +0200

    Added new menu "Festivals"
    Movies Events and Festivals now have basic UI.
    Bigger Search bar.
    Actor's age is calculated by Birthday.
    Colors of the site are flexible.

--- MovieDB-downgraded/custom-js/index.js
+++ MovieDB-downgraded/custom-js/index.js
@@ -1,5 +1,5 @@
 // Adding item to scrollable menu
-images = {
+actionMovies = {
   1: "/img/1.jpg",
   2: "/img/2.jpg",
   3: "/img/3.jpg",
@@ -11,10 +11,27 @@ images = {
   9: "/img/9.jpg",
   10: "/img/10.jpg",
 };
+comedyMovies = {
+  1: "/img/4.jpg",
+  2: "/img/3.jpg",
+  3: "/img/2.jpg",
+  4: "/img/6.jpg",
+  5: "/img/5.jpg",
+  6: "/img/7.jpg",
+  7: "/img/1.jpg",
+  8: "/img/8.jpg",
+  9: "/img/9.jpg",
+  10: "/img/10.jpg",
+};
 
-Object.keys(images).forEach(function (path) {
-  $("#img-holder").append(
-    "<div class='col-4 my-img'><img  src=" + images[path] + "></div>"
+Object.keys(actionMovies).forEach(function (path) {
+  $("#img-holder-action").append(
+    "<div class='col-4 my-img'><img  src=" + actionMovies[path] + "></div>"
+  );
+});
+Object.keys(comedyMovies).forEach(function (path) {
+  $("#img-holder-comedy").append(
+    "<div class='col-4 my-img'><img  src=" + comedyMovies[path] + "></div>"
   );
 });
 
