698698eb1814b147296be83a4a07d5514792d468
adimov <nasko312@gmail.com>
2021-01-03 23:41:32 +0200

    Deleted

--- try/storage/framework/views/f2562f11bb730426f5e03dff81947acd5c348460.php
+++
@@ -1,4 +0,0 @@
-<!-- This file is used to store topbar (left) items -->
-
-
-<?php /**PATH C:\Users\Atanas\Desktop\Project\try\resources\views/vendor/backpack/base/inc/topbar_left_content.blade.php ENDPATH**/ ?>
